c4c4ce67013d2f38e9d1ee44fb25be184c1ff4b0
Otis Wales <therunningbeta@gmail.com>
2022-08-26 14:44:05 +1200

    added global style sheet to reduce code

---
+++ src/FINAL_STYLES.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+//global styles for react-native, this contains all the global styles for the app
+
+export default StyleSheet.create({
+
+    bgPurple: { backgroundColor: "#D3A8F6" },
+    buttonColor: {
+        backgroundColor: "#9D3BEA",
+    }
+
+});
