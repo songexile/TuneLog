01ccdd2b215f70e5c57a6d1b7239a860a8ebf96b
songexile <therunningbeta@gmail.com>
2022-08-30 11:57:22 +1200

    added loading states

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -10,18 +10,24 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null); //user is initally null
+  const [loadingInital, setLoadingInital] = useState(true); //loadingInital is initally true
+  const [loading, setLoading] = useState(false); //loading is initally false
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    //This hook allows us to remember the user's login status
+    const unsub = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
       } else {
         setUser(null);
       }
+      setLoadingInital(false); //after user is fetched, loadingInital is set to false
     });
+    return unsub(); //unsubscribe from the auth listener
   }, []);
 
   const onLoginPress = (email, password) => {
+    setLoading(true); //loading is set to true
     //Takes in two arguments, email and password used in LoginScreen class
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -47,8 +53,10 @@ export const AuthProvider = ({ children }) => {
           console.warn(error);
         }
       });
+    setLoading(false); //loading is set to false
   };
   const onRegisterPress = (email, password, confirmPassword) => {
+    setLoading(true); //loading is set to true
     if (password !== confirmPassword) {
       alert("Passwords don't match.");
       return;
@@ -62,14 +70,25 @@ export const AuthProvider = ({ children }) => {
       .catch((error) => {
         alert(error);
       });
+    setLoading(false); //loading is set to false
+  };
+
+  const signOut = () => {
+    setLoading(true); //loading is set to true
+    auth.signOut();
+    console.warn(user);
+    setLoading(false); //loading is set to false
   };
 
   return (
-    <AuthContext.Provider value={{ user, onLoginPress, onRegisterPress }}>
-      {children}
+    <AuthContext.Provider
+      value={{ user, onLoginPress, onRegisterPress, signOut, loading }}
+    >
+      {!loadingInital && children}
     </AuthContext.Provider>
   );
 };
+//if loadingInital is true, we will render nothing ^
 
 export default function useAuth() {
   return useContext(AuthContext);
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { getAuth } from "firebase/auth";
+import useAuth from "../../hooks/useAuth";
 
 const auth = getAuth();
 const user = auth.currentUser;
 
 export default function HomeScreen(props) {
+  const { signOut } = useAuth();
   return (
     <View>
       <Text>Welcome to TuneLog t </Text>
+      <Button onPress={signOut} title="signout">
+        Sign out
+      </Button>
     </View>
   );
 }
