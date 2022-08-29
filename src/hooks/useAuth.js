96d2ea69140cdbdbf6b981cd4b860b0573662aaf
songexile <therunningbeta@gmail.com>
2022-08-30 09:42:37 +1200

    added sign in through useAuth hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,38 @@
 import { View, Text } from "react-native";
 import React, { createContext, useContext } from "react";
+import { auth } from "../model/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+  const onLoginPress = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+
+        const user = userCredential.user;
+        console.warn("signed in");
+        console.warn(user);
+        // ...
+      })
+      .catch((error) => {
+        //If any error we will catch
+        const errorCode = error.code;
+
+        if (errorCode === "auth/user-not-found") {
+          console.warn("User not found");
+        }
+
+        if (errorCode === "auth/wrong-password") {
+          console.warn("Wrong password");
+        } else {
+          console.warn(error);
+        }
+      });
+  };
   return (
-    <AuthContext.Provider value={{ user: null }}>
+    <AuthContext.Provider value={{ user: null, onLoginPress }}>
       {children}
     </AuthContext.Provider>
   );
