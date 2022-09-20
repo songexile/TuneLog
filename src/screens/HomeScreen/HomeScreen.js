8834e471d1deb82aeba57ccc1066be212315c7c4
Otis Wales <therunningbeta@gmail.com>
2022-09-20 13:53:29 +1200

    Revert "added boiler plate code for DB"

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -2,21 +2,18 @@ import React from "react";
 import { Button, Text, View } from "react-native";
 import { getAuth } from "firebase/auth";
 import useAuth from "../../hooks/useAuth";
-import { writeUserData } from "../../hooks/useDb";
+
+const auth = getAuth();
+const user = auth.currentUser;
 
 export default function HomeScreen(props) {
   const { signOut } = useAuth();
-
   return (
     <View>
-      <Text>Welcome to TuneLog </Text>
+      <Text>Welcome to TuneLog t </Text>
       <Button onPress={signOut} title="signout">
         Sign out
       </Button>
-      <Button
-        onPress={() => writeUserData(1, "swag", "swag")}
-        title="db"
-      ></Button>
     </View>
   );
 }
