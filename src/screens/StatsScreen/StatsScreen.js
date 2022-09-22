eefee034abe47cc1ea11d58e5ff0c21527f616a0
howorthray <91508646+howorthray@users.noreply.github.com>
2022-09-22 21:18:05 +1200

    Added feature for user to view top songs and artists

--- src/screens/StatsScreen/StatsScreen.js
+++ src/screens/StatsScreen/StatsScreen.js
@@ -1,16 +1,18 @@
 import { View, Text, Button } from "react-native";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { getSpotifyToken } from "../../hooks/spotifyAuth";
 
 const getTopTracks = async () => {
   //Getting spotify token
   const spotifyToken = getSpotifyToken();
-  // console.log("Getting access Token for TopSongs:", spotifyToken );
+  console.log("Getting access Token for TopSongs:", spotifyToken );
 
   // const [songName, setSongName] = useState("");
 
-  const api_url = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term";
+  const api_url = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5";
+  // const api_url = "https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=1&offset=5";
+  
   // console.log(api_url);
   try{
     const response = await axios.get(api_url, {
@@ -21,27 +23,37 @@ const getTopTracks = async () => {
     // console.log(response.data.items[0].name);
     console.log(response.data);
     // setSongName(response.data.items[0].name);
-    return response.data.items[0].name;
+    // const myJSON = response.data.items[0].name.toString();
+
+    return response.data.items;
   }catch(error){
     console.log(error);
   }  
 };
 
 const StatsScreen = ({ navigation }) => {
-  // const topTracks = getTopTracks();
-  // console.log(topTracks);
-  const topSong = getTopTracks();
-  // console.log(topSong);
+  const [topSong, setTopSong] = useState([]);
+  useEffect(() => {
+      getTopTracks()
+      .then(setTopSong)
+      .catch((error) => {
+          // ...handle/report error...
+      })
+  }, []); // <== Empty deps array = only on mount
 
   return (
-    <View>
-      <Button onPress={() => getTopTracks()}
-      title = "Get Top">
-        <Text>{topSong}</Text>
+      <View>
+          <Text>StatsScreen</Text>
+          {console.log(topSong)}
+          {/* {topSong && <Text>{topSong.name}</Text>} */}
 
-      </Button>
-      <Text>StatsScreen</Text>
-    </View>
+          {topSong && topSong.map((song) => (
+            <Text>{song.name} by {song.artists.map((artist) => (
+              <Text>{artist.name} </Text>
+            ))}
+            </Text>
+          ))}
+      </View>
   );
 };
 
