190d91ac5ecec3d47c3d31b01430acda5b670b75
songexile <therunningbeta@gmail.com>
2022-10-10 18:01:13 +1300

    map now gets location

--- src/screens/MusicMapScreen/MusicMapScreen.js
+++ src/screens/MusicMapScreen/MusicMapScreen.js
@@ -1,13 +1,59 @@
 import { View, Text, StyleSheet, Dimensions } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 //To be added in the future as the music map screen.
 
 const MusicMapScreen = ({ navigation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    console.warn("starting useeffect");
+    (async () => {
+      console.warn("starting async");
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        console.warn("no permission");
+        return;
+      }
+      console.warn("permission granted");
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.warn("location set");
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+    console.warn(text);
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.warn(text);
+  }
+
+  if (location == null) {
+    return (
+      <View>
+        <Text>Waiting for location</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
